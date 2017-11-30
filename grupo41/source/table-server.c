800ed1214d1d059d0e09f63982a73d75b468594b
Drannnor <chicharo.alexandre@gmail.com>
2017-11-30 17:12:53 +0000

    coisinhas do servidor

--- grupo41/source/table-server.c
+++ grupo41/source/table-server.c
@@ -17,7 +17,8 @@ Ricardo Cruz 47871
 #define NFDESC 6
 #define MAX_SIZE 1000
 
-static int quit = 0;
+static int quit = 0; 
+int primary;
 
 /* Função para preparar uma socket de receção de pedidos de ligação.
 */
@@ -218,49 +219,55 @@ int main(int argc, char **argv){
 	sigaction( SIGINT, &a, NULL );
 	signal(SIGPIPE,SIG_IGN);
 
-	if (argc < 3){
-		printf("Uso: ./server <porta TCP> <table1_size> [<table2_size> ...]\n");
-		printf("Exemplo de uso: ./table-server 54321 10 15 20 25\n");
+	if (argc >= 4){
+		primary = 1;
+		/* inicializar o n_tables*/
+		if((n_tables = (char**)malloc(sizeof(char*)*argc - 1)) == NULL){
+			fprintf(stderr, "Failed malloc tables1\n");
+			return -1;
+		}
+	
+		if((n_tables[0] = (char *)malloc(_INT)) == NULL){
+			fprintf(stderr, "Failed malloc tables2\n");
+			free(n_tables);
+			return -1;
+		}
+	
+		sprintf(n_tables[0], "%d", argc-2);
+		int count;
+		for(i = 1; i <= argc - 2; i++){
+			count = i-1;
+			if((n_tables[i] = (char *) malloc(strlen(argv[i + 1]) + 1)) == NULL){
+				while(count >= 1){
+					free(n_tables[count]);
+					count--;
+				}
+				free(n_tables);
+				fprintf(stderr, "Failed malloc tables3\n");
+				return -1;
+			}
+    		memcpy(n_tables[i],argv[i + 1],strlen(argv[i + 1]) + 1);
+		 } 
+	 
+		if((table_skel_init(n_tables) < 0)){
+			fprintf(stderr, "Failed to init\n");
+			return -1;
+		}
+
+	} else if (argc == 2){
+		primary = 0;
+	} else {
+		printf("Uso para servidor primario: ./server <porta TCP> <IP do secundario:porta TCP do secundario> <table1_size> [<table2_size> ...]\n");
+		printf("Exemplo de uso: ./server 54321 127.0.0.1:54322 10 15 20 25\n");
+		printf("-----------------------------------------------------\n");
+		printf("Uso para servidor secundario: ./server <porta TCP>\n");
+		printf("Exemplo de uso: ./server 54322\n");
 		return -1;
 	}
 
 	/* inicialização */
 	if(( socket_de_escuta = make_server_socket((unsigned short)atoi(argv[1]))) < 0){
 		fprintf(stderr, "Error creating server socket");
-		return -1;
-	}
-
-	/* inicializar o n_tables*/
-	if((n_tables = (char**)malloc(sizeof(char*)*argc - 1)) == NULL){
-		fprintf(stderr, "Failed malloc tables1\n");
-		return -1;
-	}
-
-	if((n_tables[0] = (char *)malloc(_INT)) == NULL){
-		fprintf(stderr, "Failed malloc tables2\n");
-		free(n_tables);
-		return -1;
-	}
-
-	sprintf(n_tables[0], "%d", argc-2);
-	int count;
-	for(i = 1; i <= argc - 2; i++){
-		count = i-1;
-		if((n_tables[i] = (char *) malloc(strlen(argv[i + 1]) + 1)) == NULL){
-			while(count >= 1){
-				free(n_tables[count]);
-				count--;
-			}
-			free(n_tables);
-			fprintf(stderr, "Failed malloc tables3\n");
-			return -1;
-		}
-		//verificar os mallocs TODO???
-    	memcpy(n_tables[i],argv[i + 1],strlen(argv[i + 1]) + 1);
-	 } 
-
-	if((table_skel_init(n_tables) < 0)){
-		fprintf(stderr, "Failed to init\n");
 		return -1;
 	}
 
@@ -297,7 +304,21 @@ int main(int argc, char **argv){
 					i++;
             	}
         		if ((connections[i].fd = accept(connections[0].fd, NULL, NULL)) > 0){ // Ligação feita ?
-          			connections[i].events = POLLIN; // Vamos esperar dados nesta socket
+          			connections[i].events = POLLIN;
+					if ((res = table_skel_send_tablenum(connections[i].fd)) <= 0){
+						if (res == 0){
+							close(connections[i].fd);
+							connections[i].fd = connections[nfds-1].fd;
+							connections[i].revents = connections[nfds-1].revents;
+							connections[i].events = connections[nfds-1].events;
+							connections[nfds-1].fd = -1;
+							connections[nfds-1].revents = 0;
+							connections[nfds-1].events = 0;
+						} else {
+							quit = 1;
+						}
+
+					} // Vamos esperar dados nesta socket
 					nfds++;
 				}
       	}
