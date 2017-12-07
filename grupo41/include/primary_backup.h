3e212a50405d14d23973b7ff677396ffa0fad4e2
Drannnor <chicharo.alexandre@gmail.com>
2017-12-07 15:50:21 +0000

    primary_backup.c created, ya eh isso

--- grupo41/include/primary_backup.h
+++ grupo41/include/primary_backup.h
@@ -1,9 +1,7 @@
 #ifndef _PRIMARY_BACKUP_H
 #define _PRIMARY_BACKUP_H
 
-#include "network_client-private.h"//FIXME: para tirar
-
-//struct server_t; /* Para definir em primary_backup-private.h */
+struct server_t; /* Para definir em primary_backup-private.h */
 
 /* Função usada para um servidor avisar o servidor “server” de que
 * já acordou. Retorna 0 em caso de sucesso, -1 em caso de insucesso
